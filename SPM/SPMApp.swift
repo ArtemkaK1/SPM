c2257aaeb26125eb78f8679bd4943abbe57ad9bd
Artemiy Kirillov <akirillov@rtc.usetech.ru>
2022-04-27 12:30:11 +0300

    Initial Commit

---
+++ SPM/SPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMApp.swift
+//  SPM
+//
+//  Created by Artemiy Kirillov on 27.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
