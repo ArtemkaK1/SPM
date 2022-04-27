0e6a645a8c3f94746590308cf8ca99d021e8d2dc
Artemiy Kirillov <akirillov@rtc.usetech.ru>
2022-04-27 13:29:02 +0300

    123

--- SPM/ContentView.swift
+++ SPM/ContentView.swift
@@ -6,12 +6,32 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
+    @State var email = ""
+        @State var password = ""
+
+        var body: some View {
+            VStack {
+                TextField("Email", text: $email)
+                SecureField("Password", text: $password)
+                Button(action: { login() }) {
+                    Text("Sign in")
+                }
+            }
             .padding()
-    }
+        }
+
+        func login() {
+            Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+                if error != nil {
+                    print(error?.localizedDescription ?? "")
+                } else {
+                    print("success")
+                }
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
